5a1adec5afd58b4c6d29db085b7b666c0ab7211f
Daniel Mesejo-León <mesejoleon@gmail.com>
2023-04-05 12:59:23 +0200

    new tweet-demo post

--- tweet-demo/most_repeated_element.py
+++ tweet-demo/most_repeated_element.py
@@ -3,7 +3,7 @@ from operator import itemgetter
 
 letters = ["a", "a", "b", "c", "d", "b", "a", "d"]
 max_counts = Counter(letters).most_common(1)[0]  # DON'T DO THIS
-print(max_counts)
+print(max_counts)  # ('a', 3)
 
 max_counts = max(Counter(letters).items(), key=itemgetter(1))
-print(max_counts)
+print(max_counts)  # ('a', 3)
