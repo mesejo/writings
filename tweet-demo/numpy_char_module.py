ab3b3b585fdd7465f41366ad3b9c57db6e3dd9fb
Daniel Mesejo-León <mesejoleon@gmail.com>
2023-04-14 11:44:17 +0200

    two more demos

---
+++ tweet-demo/numpy_char_module.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+res = np.char.capitalize(["python", "numpy"])
+print(res)  # ['Python' 'Numpy']
---
+++ tweet-demo/truthy_values.py
@@ -0,0 +1,5 @@
+res = "world" or "hello"
+print("result:", res)  # result: world, if x is true, then x, else y
+
+res = [] and "hello"
+print("result:", res)  # result: [], if x is false, then x, else y
